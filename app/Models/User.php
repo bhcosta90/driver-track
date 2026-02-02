3c2a746611a56e57bcff2cac94171d4c84dda23b
Bruno Costa <bhcosta90@gmail.com>
2026-02-02 13:10:11 -0300

    Add docblock properties to User model for improved code documentation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,19 @@ declare(strict_types = 1);
 
 namespace App\Models;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property string               $name
+ * @property string               $email
+ * @property string               $password
+ * @property CarbonImmutable|null $email_verified_at
+ * @property CarbonImmutable|null $created_at
+ * @property CarbonImmutable|null $updated_at
+ */
 final class User extends Authenticatable
 {
     use HasFactory;
